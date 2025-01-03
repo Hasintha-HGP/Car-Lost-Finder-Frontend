6fc718dc819af2203eedb3415065e460a5ef06e0
supunsb21 <supunsb21@gmail.com>
2025-01-03 12:57:23 +0530

    Create Add Car & Add Garage Components

--- Car_Lost/src/App.jsx
+++ Car_Lost/src/App.jsx
@@ -1,20 +1,18 @@
 import Home from './Commponent/Home/Home.jsx';
 import About from './Commponent/About/About.jsx';
 import Update from './Commponent/Update/Update.jsx';
-
-
-
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import Login from './Commponent/Login/Login.jsx';
-
+import AddCar from './Commponent/AddCar/AddCar.jsx';
+import AddGarage from './Commponent/AddGarage/AddGarage.jsx';
 
 function App() {
   
 
   return (
     <>
-      <About/>
+      
     </>
   )
 }
